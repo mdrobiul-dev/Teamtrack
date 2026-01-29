1d4e5cd46edd67664c96045682424630270b140f
its-robii <robiulhassanrobi33@gmail.com>
2026-01-29 13:38:21 +0600

    All models created

--- backend/middlewear/auth.middleware.js
+++ backend/middlewear/auth.middleware.js
@@ -11,8 +11,8 @@ const protected = (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = decoded;
+      
       next();
     } catch (error) {
       return res.status(401).json({ message: "Not authorized" });
