08068018fb3d0a46f4f73b8896d79c4f175c17cb
its-robii <robiulhassanrobi33@gmail.com>
2026-02-11 18:35:39 +0600

    update

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-// this is all the routes 
+// this is all the routes
 const authRoutes = require("./routes/auth.routes");
-const workspaceRoutes  = require("./routes/workspace.routes")
-const boardRoutes  = require("./routes/board.routes")
-const listRoutes = require("./routes/list.routes")
-const taskRoutes  = require("./routes/task.routes")
-const activityRoutes = require("./routes/activity.routes")
-
+const workspaceRoutes = require("./routes/workspace.routes");
+const boardRoutes = require("./routes/board.routes");
+const listRoutes = require("./routes/list.routes");
+const taskRoutes = require("./routes/task.routes");
+const activityRoutes = require("./routes/activity.routes");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -18,15 +17,13 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-
-// this is all the routes 
+// this is all the routes
 app.use("/api/auth", authRoutes);
-app.use("/api/workspaces", workspaceRoutes)
-app.use("/api/boards", boardRoutes)
-app.use("/api/lists", listRoutes)
-app.use("/api/tasks", taskRoutes )
-app.use("/api/activity", activityRoutes)
-
+app.use("/api/workspaces", workspaceRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api/lists", listRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/activity", activityRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -38,4 +35,4 @@ mongoose
   })
   .catch((err) => {
     console.error("DB connection failed", err);
-  });                             
+  });
