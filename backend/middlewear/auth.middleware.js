f1a981c24d0355e90a725297624029593e14badb
its-robii <robiulhassanrobi33@gmail.com>
2026-02-15 20:28:29 +0600

    auth controller polish

--- backend/middlewear/auth.middleware.js
+++ backend/middlewear/auth.middleware.js
@@ -12,7 +12,7 @@ const protected = (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded;
-      
+
       next();
     } catch (error) {
       return res.status(401).json({ message: "Not authorized" });
@@ -22,4 +22,4 @@ const protected = (req, res, next) => {
   }
 };
 
-module.exports = protected;
+module.exports = protected;   
