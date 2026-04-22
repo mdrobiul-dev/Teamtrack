a76abfa9423b4c30b4475fa6b9b34df4fc740abc
its-robii <robiulhassanrobi33@gmail.com>
2026-04-22 19:28:14 +0600

    connected backendt to front end apiss

--- frontend/app/types/workspace.ts
+++ frontend/app/types/workspace.ts
@@ -29,4 +29,4 @@ export interface List {
   order: number;
   createdAt: string;
   updatedAt: string;
-}                                                                                          
+}                                                                                                        
